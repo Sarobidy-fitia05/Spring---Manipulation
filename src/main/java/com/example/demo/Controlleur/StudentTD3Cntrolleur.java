61184039ca985b39da82fd26f6cae6b8d57b9f97
sarobidy <hei.sarobidy.6pgmail.com>
2026-03-20 18:07:53 +0300

    feat: setup TD3 controller and updated entity

--- src/main/java/com/example/demo/Controlleur/StudentTD3Cntrolleur.java
+++ src/main/java/com/example/demo/Controlleur/StudentTD3Cntrolleur.java
@@ -9,19 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/td3")
 public class StudentTD3Cntrolleur {
 
     private final List<Student> studentList = new ArrayList<>();
 
-    @GetMapping("/td3")
-    public ResponseEntity<String> welcome (@RequestParam(value = "name", required = false) String name) {
+    @GetMapping("/welcome")
+    public ResponseEntity<String> td3 (@RequestParam(value = "name", required = false) String name) {
         if (name == null || name.isEmpty()){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Le paramètre 'name' est obligatoire.");
             }
         return ResponseEntity.ok("Welcome " + name);
     }
-    @PostMapping("/td3")
-    public ResponseEntity<List<Student>> addStudents(@RequestBody List<Student> students) {
+    @PostMapping("/students")
+    public ResponseEntity<List<Student>> addStudentsTD3(@RequestBody List<Student> students) {
         try {
             this.studentList.addAll(students);
             return ResponseEntity.status(HttpStatus.CREATED).body(this.studentList);
@@ -30,8 +31,8 @@ public class StudentTD3Cntrolleur {
         }
     }
 
-    @GetMapping("/td3")
-    public ResponseEntity<?> getStudents(@RequestHeader(value = "Accept" , required = false) String accept) {
+    @GetMapping("/students")
+    public ResponseEntity<?> getStudentsTd3(@RequestHeader(value = "Accept" , required = false) String accept) {
         try {
             if (accept == null) {
                 return  ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
@@ -39,9 +40,9 @@ public class StudentTD3Cntrolleur {
             if (accept.contains("text/plain") || accept.contains("application json")) {
                 return ResponseEntity.ok(this.studentList) ;
             }
-            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build()
+            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
